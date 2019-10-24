c718028d9d87443aebf68a45e62f4d86d5d086e4
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-24 19:41:16 +0200

    catching form elements and definig errors messages

--- src/js/form-validate.js
+++ src/js/form-validate.js
@@ -1,8 +1,27 @@
 var formValidations = {
     required: {
-      
-    },
-    email: {
-      
-    }
+        validation: function (value) {
+          return value !== '';
+        },
+        error: 'Obavezno polje.'
+      },
+      email: {
+        validation: function (value) {
+          return value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        },
+        error: 'Neispravna e-mail adresa.'
+      }
+  }
+  function valitation () {
+      let $form = $('#form');
+      let $inputs = $form.find('input, textarea');
+
+      $form.on('submit', function (e) {
+        e.preventDefault();
+        $inputs.each(function () {
+            let $this = $(this);
+            rule = $this.data('validate') ? $this.data('validate') : '',
+            $parent = $this.parent(".form-container__form--el");
+        })
+      })
   }
